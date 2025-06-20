aa5b7bf1b3f38e612e480393f52a2d7699b942c7
Adrian Arias <sauladrian.arias@gmail.com>
2025-06-20 16:24:48 -0600

    a ver si funciona

--- app/Http/Controllers/AlmacenController.php
+++ app/Http/Controllers/AlmacenController.php
@@ -41,6 +41,7 @@ use Picqer\Barcode\BarcodeGeneratorJPG;
 use Picqer\Barcode\Exceptions\BarcodeException;
 use stdClass;
 use Throwable;
+use Crabbly\Fpdf\Fpdf;
 
 class AlmacenController extends Controller
 {
@@ -2358,7 +2359,7 @@ class AlmacenController extends Controller
         $pageline = 3;
         $anchotexto = 60;
 
-        $pdf = app('FPDF');
+        $pdf = new Fpdf();
 
         $pdf->AddPage('L', 'Letter');
         $pdf->SetFont('Arial', '', $fontsize);
