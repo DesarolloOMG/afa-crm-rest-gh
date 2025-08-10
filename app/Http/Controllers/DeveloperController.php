cfab442e8aae3f037436d5ea74add8ad9871de23
Adrian Arias <sauladrian.arias@gmail.com>
2025-08-10 03:52:03 -0600

    primero entradas

--- app/Http/Controllers/DeveloperController.php
+++ app/Http/Controllers/DeveloperController.php
@@ -191,7 +191,7 @@ class DeveloperController extends Controller
 
         $documentos = DB::table('documento')
             ->select('id', 'id_tipo', 'autorizado')
-            ->whereIn('id_tipo', [0, 2, 3, 4, 5, 11])
+            ->whereIn('id_tipo', [0, 3])
             ->whereIn('id_fase', [5, 6, 100, 607])
             ->where('status', 1)
             ->orderBy('id', 'asc')
