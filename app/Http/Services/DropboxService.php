0947d201ad876157b2a85eec70dee24d908a5849
Adrian Arias <sauladrian.arias@gmail.com>
2025-12-15 12:52:29 -0600

    quita llamada redundante a ensureValidToken

--- app/Http/Services/DropboxService.php
+++ app/Http/Services/DropboxService.php
@@ -173,7 +173,6 @@ class DropboxService
      */
     public function downloadFile($path): ?string
     {
-        $this->ensureValidToken();
         $token = $this->getDropboxToken();
 
         $url = 'https://content.dropboxapi.com/2/files/download';
@@ -215,7 +214,6 @@ class DropboxService
      */
     public function uploadFile($path, $fileContent, $isBase64 = true)
     {
-        $this->ensureValidToken();
         $token = $this->getDropboxToken();
 
         $url = 'https://content.dropboxapi.com/2/files/upload';
@@ -281,7 +279,6 @@ class DropboxService
      */
     private function requestDropbox($url, $body = [], $asJson = false)
     {
-        $this->ensureValidToken();
         $token = $this->getDropboxToken();
 
         $headers = [
